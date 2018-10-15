de8b76fed67a95a33933bfd5a7851572e7a8ae00
Branden Kim <brandenk0@gmail.com>
2018-10-15 01:37:06 -0400

    Skeleton for api abstraction

---
+++ app/services/api_sonos.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class SonosAPI():
+    
+    def __init__():
+        # self.access_token = 
+        pass
+    
+    def _get_access_token():
+        pass
+    
+    def get(url):
+        pass
+
+    def post(url, data):
+        pass
