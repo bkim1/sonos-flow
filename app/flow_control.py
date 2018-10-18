ddcb4946bc66266bdb67553a9684d45c41a9a702
Branden Kim <brandenk0@gmail.com>
2018-10-17 20:11:29 -0400

    Add continue endpoint & switch to 'txt' db

--- app/flow_control.py
+++ app/flow_control.py
@@ -1,5 +1,7 @@
 import os
 import random
+import time
+import ast
 
 from flask import (
     Blueprint, g, redirect, render_template, request, jsonify
@@ -10,10 +12,9 @@ from app.services.api_sonos import SonosAPI
 
 bp = Blueprint('flow_control', __name__, url_prefix='/flow')
 SONOS_API = SonosAPI()
-GROUP_IDS = {}
-PLAYER_IDS = {}
-FAVORITES = {}
-
+GROUP_IDS_FILE = 'app/db/group_ids.txt'
+PLAYER_IDS_FILE = 'app/db/player_ids.txt'
+FAVORITES_FILE = 'app/db/favorites.txt'
 
 @bp.route('/', methods=['GET'])
 def setup_flow():
@@ -36,24 +37,17 @@ def setup_flow():
     if code < 200 or code > 300:
         return f'Error with getting groups: {data}\nStatus Code: {code}'
     
-    for group in data['groups']:
-        GROUP_IDS[group['name']] = group['id']
-        PLAYER_IDS[group['name']] = group['playerIds']
-
+    set_saved_groups(data['groups'])
+    set_saved_players(data['groups'])
+    
     # Get favorites
     data, code = SONOS_API.get(f'households/{household_id}/favorites')
     if code < 200 or code > 300:
         return f'Error with getting favorites: {data}\nStatus Code: {code}'
 
-    for item in data['items']:
-        FAVORITES[item['name']] = item['id']
+    set_saved_favorites(data['items'])
 
-    return jsonify({
-        'message': 'Flow has been setup!',
-        'favorites': FAVORITES,
-        'groups': GROUP_IDS,
-        'players': PLAYER_IDS
-    })
+    return jsonify({ 'message': 'Flow has been setup!' })
 
 
 @bp.route('/favorites', methods=['GET'])
@@ -69,8 +63,7 @@ def get_favorites():
         if code < 200 or code > 300:
             return f'Error with getting favorites: {data}\nStatus Code: {code}'
 
-        for item in data['items']:
-            FAVORITES[item['name']] = item['id']
+        set_saved_favorites(data['items'])
         return jsonify({'message': 'Got the favorites!', 'data': data['items']})
 
 
@@ -93,12 +86,12 @@ def enter_flow(group, favorite):
     fav_provided = True
     try: 
         household_id = os.getenv('HouseholdID')
-        group_id = GROUP_IDS[group]
-        
+        group_id = get_saved_groups()[group]
+        favorites = get_saved_favorites()
         if favorite is None:
-            favorite = random.choice(list(FAVORITES))
+            favorite = random.choice(list(favorites))
             fav_provided = False
-        favorite_id = FAVORITES[favorite]
+        favorite_id = favorites[favorite]
     except KeyError:
         return 'Need to setup the flow first!'
     else:
@@ -115,7 +108,7 @@ def enter_flow(group, favorite):
         }
         data, code = SONOS_API.post(f'groups/{group_id}/favorites', payload=payload)
         if code < 200 or code > 300:
-            return f'Error with starting playback: {data}\nStatus Code: {code}'
+            return jsonify({ 'error': data, 'code': code, 'message': 'play' })
 
         return jsonify({'message': 'Entered flow!', 'data': data})
 
@@ -129,20 +122,61 @@ def exit_flow(group):
             group: string representing the name of the group in the household
     """
     try: 
-        group_id = GROUP_IDS[group]
+        group_id = get_saved_groups()[group]
     except KeyError:
         return 'Need to setup the flow first!'
     else:
         data, code = SONOS_API.post(f'groups/{group_id}/playback/pause')
         if code < 200 or code > 300:
-            return f'Error with pausing playback: {data}\nStatus Code: {code}'
+            return jsonify({ 'error': data, 'code': code, 'message': 'pause' })
 
         return jsonify({'message': 'Exited flow!', 'data': data})
 
 
 @bp.route('/continue/<string:group_from>/<string:group_to>', methods=['GET'])
 def continue_flow(group_from, group_to):
-    pass
+    # Attempt to group together the speakers & then remove one of them
+    try:
+        groups, players = get_saved_groups(), get_saved_players()
+        id_from, id_to = groups[group_from], groups[group_to]
+        players_from, players_to = players[group_from], players[group_to]
+    except KeyError as err:
+        return jsonify({
+            'error': str(err),
+            'message': 'Need to setup the flow first!'
+        })
+    else:
+        # Add new group to existing one
+        payload = {
+            'playerIdsToAdd': players_to,
+            'playerIdsToRemove': []
+        }
+        data, code = SONOS_API.post(f'groups/{id_from}/groups/modifyGroupMembers', payload=payload)
+        if code < 200 or code > 300:
+            return jsonify({ 'error': data, 'code': code, 'message': 'add group' })
+
+        # Update the group ids
+        get_groups()
+        groups = get_saved_groups()
+        new_id_from = groups[f'{group_from} + 1']
+        print(new_id_from)
+
+        # Remove previous group
+        payload = {
+            'playerIdsToAdd': [],
+            'playerIdsToRemove': players_from
+        }
+        data, code = SONOS_API.post(f'groups/{new_id_from}/groups/modifyGroupMembers', payload=payload)
+        if code < 200 or code > 300:
+            return jsonify({ 'error': data, 'code': code, 'message': 'remove group' })
+
+        # Update the group ids again
+        get_groups()
+        return jsonify({
+            'message': f'Transitioned from {group_from} to {group_to}',
+            'data': data
+        })
+
 
 
 @bp.route('/groups', methods=['GET'])
@@ -155,7 +189,10 @@ def get_groups():
         # Get groups & player IDs
         data, code = SONOS_API.get(f'households/{household_id}/groups')
         if code < 200 or code > 300:
-            return f'Error with getting groups: {data}\nStatus Code: {code}'
+            return jsonify({ 'error': data, 'code': code, 'message': 'get group' })
+        
+        # Update group info
+        set_saved_groups(data['groups'])
         
         return jsonify({'data': data, 'message': 'Got the groups!'})
 
@@ -168,3 +205,54 @@ def handle_refresh():
     if resp:
         return 'Refresh worked!'
     return 'Refresh Failed!'
+
+
+def get_saved_groups():
+    groups = {}
+    with open(GROUP_IDS_FILE, 'r') as f:
+        for line in f:
+            key, value = line.split(',,')
+            groups[key] = value.rstrip()
+    print(groups)
+    return groups
+
+
+def set_saved_groups(groups):
+    with open(GROUP_IDS_FILE, 'w+') as f:
+        for group in groups:
+            f.write(f'{group["name"]},,{group["id"]}\n')
+    return True
+
+
+def get_saved_favorites():
+    favorites = {}
+    with open(FAVORITES_FILE, 'r') as f:
+        for line in f:
+            key, value = line.split(',,')
+            favorites[key] = value.rstrip()
+    print(favorites)
+    return favorites
+
+
+def set_saved_favorites(favorites):
+    with open(FAVORITES_FILE, 'w+') as f:
+        for item in favorites:
+            f.write(f'{item["name"]},,{item["id"]}\n')
+    return True
+
+
+def get_saved_players():
+    players = {}
+    with open(PLAYER_IDS_FILE, 'r') as f:
+        for line in f:
+            key, value = line.split(',,')
+            players[key] = ast.literal_eval(value.rstrip())
+    print(players)
+    return players        
+
+
+def set_saved_players(groups):
+    with open(PLAYER_IDS_FILE, 'w+') as f:
+        for group in groups:
+            f.write(f'{group["name"]},,{str(group["playerIds"])}\n')
+    return True
